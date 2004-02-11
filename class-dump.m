8ca5670455befbf0ce22af87b201c814bf52df91
Steve Nygard <nygard@gmail.com>
2004-02-11 00:55:34 +0000

    Removed some more old code, and class-dump.h entirely.

--- class-dump.m
+++ class-dump.m
@@ -28,161 +28,9 @@
 #import <Foundation/Foundation.h>
 #import "NSString-Extensions.h"
 
-#import "class-dump.h"
 #import "CDClassDump.h"
 
-RCS_ID("$Header: /Volumes/Data/tmp/Tools/class-dump/Attic/class-dump.m,v 1.73 2004/02/11 00:07:54 nygard Exp $");
-
-//======================================================================
-
-char *file_type_names[] =
-{
-    "MH_<unknown>",
-    "MH_OBJECT",
-    "MH_EXECUTE",
-    "MH_FVMLIB",
-    "MH_CORE",
-    "MH_PRELOAD",
-    "MH_DYLIB",
-    "MH_DYLINKER",
-    "MH_BUNDLE",
-};
-
-char *load_command_names[] =
-{
-    "LC_<unknown>",
-    "LC_SEGMENT",
-    "LC_SYMTAB",
-    "LC_SYMSEG",
-    "LC_THREAD",
-    "LC_UNIXTHREAD",
-    "LC_LOADFVMLIB",
-    "LC_IDFVMLIB",
-    "LC_IDENT",
-    "LC_FVMFILE",
-    "LC_PREPAGE",
-    "LC_DYSYMTAB",
-    "LC_LOAD_DYLIB",
-    "LC_ID_DYLIB",
-    "LC_LOAD_DYLINKER",
-    "LC_ID_DYLINKER",
-    "LC_PREBOUND_DYLIB",
-    "LC_ROUTINES",
-    "LC_SUB_FRAMEWORK",
-};
-
-void print_header(void);
-
-//======================================================================
-#if 0
-@implementation CDClassDump
-
-- (void)processDylibCommand:(void *)start ptr:(void *)ptr;
-{
-    struct dylib_command *dc = (struct dylib_command *)ptr;
-    NSString *str;
-    char *strptr;
-
-    strptr = ptr + dc->dylib.name.offset;
-    str = [[NSFileManager defaultManager] stringWithFileSystemRepresentation:strptr length:strlen(strptr)];
-    //NSLog(@"strptr: '%s', str: '%@'", strptr, str);
-    [self buildUpObjectiveCSegments:str];
-}
-
-- (void)processFvmlibCommand:(void *)start ptr:(void *)ptr;
-{
-    struct fvmlib_command *fc = (struct fvmlib_command *)ptr;
-    NSString *str;
-    char *strptr;
-
-    strptr = ptr + fc->fvmlib.name.offset;
-    str = [[NSFileManager defaultManager] stringWithFileSystemRepresentation:strptr length:strlen(strptr)];
-    NSLog(@"strptr: '%s', str: '%@'", strptr, str);
-    [self buildUpObjectiveCSegments:str];
-}
-
-//======================================================================
-
-- (void)showSingleModule:(CDSectionInfo *)moduleInfo;
-{
-    struct my_objc_module *m;
-    int module_count;
-    int l;
-    NSString *tmp;
-    id en, thing, key;
-    NSMutableArray *classList;
-    NSArray *newClasses;
-    int formatFlags;
-
-    if (moduleInfo == nil)
-        return;
-
-    classList = [NSMutableArray array];
-    formatFlags = [self methodFormattingFlags];
-
-    tmp = current_filename;
-    m = [moduleInfo start];
-    module_count = [moduleInfo size] / sizeof(struct my_objc_module);
-
-    {
-        MappedFile *currentFile;
-        NSString *installName, *filename;
-        NSString *key;
-
-        key = [moduleInfo filename];
-        currentFile = [mappedFilesByInstallName objectForKey:key];
-        installName = [currentFile installName];
-        filename = [currentFile filename];
-        if (flags.shouldGenerateHeaders == NO) {
-            if (filename == nil || [installName isEqual:filename] == YES) {
-                printf("\n/*\n * File: %s\n */\n\n", [installName fileSystemRepresentation]);
-            } else {
-                printf("\n/*\n * File: %s\n * Install name: %s\n */\n\n", [filename fileSystemRepresentation], [installName fileSystemRepresentation]);
-            }
-        }
-
-        current_filename = key;
-    }
-    //current_filename = module_info->filename;
-
-    for (l = 0; l < module_count; l++) {
-        newClasses = [self handleObjectiveCSymtab:(struct my_objc_symtab *)[self translateAddressToPointer:m->symtab section:CDSECT_SYMBOLS]];
-        [classList addObjectsFromArray:newClasses];
-        m++;
-    }
-
-    if (flags.shouldSort == YES)
-        en = [[[protocols allKeys] sortedArrayUsingSelector:@selector(compare:)] objectEnumerator];
-    else
-        en = [[protocols allKeys] objectEnumerator];
-
-    while (key = [en nextObject]) {
-        thing = [protocols objectForKey:key];
-        if (flags.shouldMatchRegex == NO || [self regexMatchesCString:[[thing sortableName] cString]] == YES)
-            [thing showDefinition:formatFlags];
-    }
-
-    if (flags.shouldSort == YES && flags.shouldSortClasses == NO)
-        en = [[classList sortedArrayUsingSelector:@selector(orderByName:)] objectEnumerator];
-    else if (flags.shouldSortClasses == YES)
-        en = [[ObjcClass sortedClasses] objectEnumerator];
-    else
-        en = [classList objectEnumerator];
-
-    while (thing = [en nextObject]) {
-        if (flags.shouldMatchRegex == NO || [self regexMatchesCString:[[thing sortableName] cString]] == YES) {
-            [thing showDefinition:formatFlags];
-        }
-    }
-
-    [protocols removeAllObjects];
-
-    current_filename = tmp;
-}
-
-@end
-#endif
-//----------------------------------------------------------------------
+RCS_ID("$Header: /Volumes/Data/tmp/Tools/class-dump/Attic/class-dump.m,v 1.74 2004/02/11 00:55:34 nygard Exp $");
 
 void print_usage(void)
 {
@@ -205,8 +53,6 @@ void print_usage(void)
        );
 }
 
-//======================================================================
-
 extern int optind;
 extern char *optarg;
 
@@ -224,7 +70,7 @@ int main(int argc, char *argv[])
     }
 
     classDump = [[[CDClassDump alloc] init] autorelease];
-    [classDump setOutputPath:@"/tmp/cd"];
+    [classDump setOutputPath:@"/tmp/cd"]; // TODO (2004-02-10): Need to remove this
 
     while ( (ch = getopt(argc, argv, "aAC:HIo:rRsS")) != EOF) {
         switch (ch) {
