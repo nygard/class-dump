7cb2e54c4f272c8db0f03361998d129fd5f62df2
Steve Nygard <nygard@gmail.com>
2004-02-11 01:08:18 +0000

    Trimmed copyright.

--- main.m
+++ main.m
@@ -1,25 +1,6 @@
-//
 //  This file is a part of class-dump v2, a utility for examining the
 //  Objective-C segment of Mach-O files.
 //  Copyright (C) 1997-1998, 2000-2001, 2004  Steve Nygard
-//
-//  This program is free software; you can redistribute it and/or modify
-//  it under the terms of the GNU General Public License as published by
-//  the Free Software Foundation; either version 2 of the License, or
-//  (at your option) any later version.
-//
-//  This program is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU General Public License for more details.
-//
-//  You should have received a copy of the GNU General Public License
-//  along with this program; if not, write to the Free Software
-//  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-//
-//  You may contact the author by:
-//     e-mail:  class-dump at codethecode.com
-//
 
 #include <stdio.h>
 #include <libc.h>
@@ -30,7 +11,7 @@
 
 #import "CDClassDump.h"
 
-RCS_ID("$Header: /Volumes/Data/tmp/Tools/class-dump/main.m,v 1.3 2004/02/11 01:03:39 nygard Exp $");
+RCS_ID("$Header: /Volumes/Data/tmp/Tools/class-dump/main.m,v 1.4 2004/02/11 01:08:18 nygard Exp $");
 
 void print_usage(void)
 {
