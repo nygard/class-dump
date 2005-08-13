d4f7e3eb3f3e23325067e65de7543960a3ec90cb
Steve Nygard <nygard@gmail.com>
2005-08-13 20:32:02 +0000

    Updated copyrights again, reformatting the description into one line, and making sure all the copyrights are the same.

--- CDTypeParser.m
+++ CDTypeParser.m
@@ -1,5 +1,4 @@
-//  This file is part of class-dump, a utility for examining the
-//  Objective-C segment of Mach-O files.
+//  This file is part of class-dump, a utility for examining the Objective-C segment of Mach-O files.
 //  Copyright (C) 1997-1998, 2000-2001, 2004-2005  Steve Nygard
 
 #import "CDTypeParser.h"
