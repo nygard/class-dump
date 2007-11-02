58f40520b9ec41598a2cecc7fba6fa6c30a4c906
Steve Nygard <nygard@gmail.com>
2007-11-02 09:46:37 +0000

    Updated copyrights.  Updated path to web page (lower case projects now).

--- CDMultiFileVisitor.h
+++ CDMultiFileVisitor.h
@@ -1,7 +1,7 @@
 // -*- mode: ObjC -*-
 
 //  This file is part of class-dump, a utility for examining the Objective-C segment of Mach-O files.
-//  Copyright (C) 2007 Steve Nygard.  All rights reserved.
+//  Copyright (C) 1997-1998, 2000-2001, 2004-2007  Steve Nygard
 
 #import "CDTextClassDumpVisitor.h"
 
