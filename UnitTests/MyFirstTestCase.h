8f5d3fbfaab82eafbe09a4f5be9160df369d340b
Steve Nygard <nygard@gmail.com>
2007-11-02 17:07:49 +0000

    Removed my old ObjcUnit target, since it doesn't seem to work on Leopard and I don't want to try to figure out what's going wrong. Plus it's using a bunch of deprecated API.  I'll try to use the built-in OCUnit/SenTestKit instead.  Added one test to test it.

---
+++ UnitTests/MyFirstTestCase.h
@@ -0,0 +1,18 @@
+//
+//  MyFirstTestCase.h
+//  class-dump
+//
+//  Created by Steve Nygard on 2007-11-02.
+//  Copyright 2007 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface MyFirstTestCase : SenTestCase
+{
+}
+
+- (void)testSomething;
+
+@end
---
+++ UnitTests/MyFirstTestCase.m
@@ -0,0 +1,22 @@
+//
+//  MyFirstTestCase.m
+//  class-dump
+//
+//  Created by Steve Nygard on 2007-11-02.
+//  Copyright 2007 __MyCompanyName__. All rights reserved.
+//
+
+#import "MyFirstTestCase.h"
+
+
+@implementation MyFirstTestCase
+
+- (void)testSomething;
+{
+	NSLog(@"%s", __PRETTY_FUNCTION__);
+    STAssertEquals(32, 32,
+                   @"Centigrade freezing point should be 32, but was %d instead!", 
+                   32);
+}
+
+@end
