fd87bd9d8a943be8d4890a372063e65438baf216
Steve Nygard <nygard@gmail.com>
2007-11-05 17:29:07 +0000

    Removed old bit of code.

--- UnitTests/AllTests.m
+++ UnitTests/AllTests.m
@@ -106,21 +106,4 @@
     return allTests;
 }
 
-#if 0
-+ (TestSuite *)suite;
-{
-    TestSuite *suite;
-
-    suite = [TestSuite suiteWithName:@"My Tests"];
-
-    // Add your tests here...
-    [suite addTest:[TestSuite suiteWithClass:[CDTypeLexerUnitTest class]]];
-    [suite addTest:[TestSuite suiteWithClass:[CDTypeParserUnitTest class]]];
-    [suite addTest:[TestSuite suiteWithClass:[CDTypeFormatterUnitTest class]]];
-    [suite addTest:[TestSuite suiteWithClass:[CDStructHandlingUnitTest class]]];
-    [suite addTest:[TestSuite suiteWithClass:[CDPathUnitTest class]]];
-    return suite;
-}
-#endif
-
 @end
