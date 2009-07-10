b834121ee543ecf6fd40cdb87ad0b37c8f680cee
Steve Nygard <nygard@gmail.com>
2009-07-10 12:40:54 +0000

    It should now be easier to add balance pairs.

--- CDBalanceFormatter.m
+++ CDBalanceFormatter.m
@@ -9,8 +9,6 @@
 
 static BOOL debug = NO;
 
-// TODO: Add union (, array [
-
 @implementation CDBalanceFormatter
 
 - (id)initWithString:(NSString *)str;
@@ -43,7 +41,8 @@ static BOOL debug = NO;
     NSString *opens[] = { @"{", @"<", @"(", nil};
     NSString *closes[] = { @"}", @">", @")", nil};
     NSUInteger index;
-    BOOL foundSomething;
+    BOOL foundOpen = NO;
+    BOOL foundClose = NO;
 
     pairs = [[NSArray alloc] initWithObjects:@"{}", @"<>", @"()", nil];
 
@@ -52,9 +51,11 @@ static BOOL debug = NO;
             if (debug) NSLog(@"pre = '%@'", pre);
             [result appendFormat:@"%@%@\n", [NSString spacesIndentedToLevel:level], pre];
         }
+        if (debug) NSLog(@"remaining: '%@'", [[scanner string] substringFromIndex:[scanner scanLocation]]);
 
-        foundSomething = NO;
+        foundOpen = foundClose = NO;
         for (index = 0; index < 3; index++) {
+            if (debug) NSLog(@"Checking open %u: '%@'", index, opens[index]);
             if ([scanner scanString:opens[index] intoString:NULL]) {
                 if (debug) NSLog(@"Start %@", opens[index]);
                 [result appendSpacesIndentedToLevel:level];
@@ -66,25 +67,29 @@ static BOOL debug = NO;
                 [result appendSpacesIndentedToLevel:level];
                 [result appendString:closes[index]];
                 [result appendString:@"\n"];
-                foundSomething = YES;
+                foundOpen = YES;
                 break;
             }
 
+            if (debug) NSLog(@"Checking close %u: '%@'", index, closes[index]);
             if ([scanner scanString:closes[index] intoString:NULL]) {
                 if ([open isEqualToString:opens[index]]) {
                     if (debug) NSLog(@"End %@", closes[index]);
                 } else {
                     NSLog(@"ERROR: Unmatched end %@", closes[index]);
                 }
-                foundSomething = YES;
+                foundClose = YES;
                 break;
             }
         }
 
-        if (foundSomething == NO) {
+        if (foundOpen == NO && foundClose == NO) {
             if (debug) NSLog(@"Unknown @ %u: %@", [scanner scanLocation], [[scanner string] substringFromIndex:[scanner scanLocation]]);
             break;
         }
+
+        if (foundClose)
+            break;
     }
 
     [pairs release];
@@ -92,36 +97,7 @@ static BOOL debug = NO;
 
 - (NSString *)format;
 {
-    NSString *pre;
-    NSUInteger level = 0;
-
-    if ([scanner scanUpToCharactersFromSet:openCloseSet intoString:&pre]) {
-        if (debug) NSLog(@"pre = '%@'", pre);
-        [result appendFormat:@"%@\n", pre];
-    }
-
-    if ([scanner scanString:@"{" intoString:NULL]) {
-        if (debug) NSLog(@"Start {");
-        [result appendFormat:@"{\n"];
-        [self parse:@"{" index:[scanner scanLocation] - 1 level:level + 1];
-        [result appendFormat:@"}\n"];
-    } else if ([scanner scanString:@"}" intoString:NULL]) {
-        NSLog(@"ERROR: Unmatched end }");
-    } else if ([scanner scanString:@"<" intoString:NULL]) {
-        if (debug) NSLog(@"Start <");
-        [result appendFormat:@"<\n"];
-        [self parse:@"<" index:[scanner scanLocation] - 1 level:level + 1];
-        [result appendFormat:@">\n"];
-    } else if ([scanner scanString:@">" intoString:NULL]) {
-        NSLog(@"ERROR: Unmatched end >");
-    } else if ([scanner scanString:@"(" intoString:NULL]) {
-        if (debug) NSLog(@"Start (");
-        [result appendFormat:@"(\n"];
-        [self parse:@"(" index:[scanner scanLocation] - 1 level:level + 1];
-        [result appendFormat:@")\n"];
-    } else if ([scanner scanString:@")" intoString:NULL]) {
-        NSLog(@"ERROR: Unmatched end )");
-    }
+    [self parse:nil index:0 level:0];
 
     if (debug) NSLog(@"result:\n%@", result);
 
