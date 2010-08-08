5cf803eb11a64eff482cf36325ddd1ba316a7c37
Steve Nygard <nygard@gmail.com>
2010-08-08 01:51:12 -0600

    Updated description of --sdk-root in usage text.

--- class-dump.m
+++ class-dump.m
@@ -44,7 +44,7 @@ void print_usage(void)
             "        -S             sort methods by name\n"
             "        -t             suppress header in output, for testing\n"
             "        --list-arches  list the arches in the file, then exit\n"
-            "        --sdk-root     specify the SDK root path (full path, or 3.0, 3.1, 3.1.2, 3.1.3, 3.2, 4.0, 10.5, 10.6)\n"
+            "        --sdk-root     specify the SDK root path (full path, or 4.1, 4.0, 3.2, 10.6, 10.5, 3.1.3, 3.1.2, 3.1)\n"
             ,
             CLASS_DUMP_VERSION
        );
