4dd0196fb053b58c87f13566da02b8f3744d87ef
Steve Nygard <nygard@gmail.com>
2011-05-13 01:25:11 -0600

    Updated copyrights.

--- CDClassFrameworkVisitor.m
+++ CDClassFrameworkVisitor.m
@@ -1,7 +1,7 @@
 // -*- mode: ObjC -*-
 
 //  This file is part of class-dump, a utility for examining the Objective-C segment of Mach-O files.
-//  Copyright (C) 1997-1998, 2000-2001, 2004-2010 Steve Nygard.
+//  Copyright (C) 1997-1998, 2000-2001, 2004-2011 Steve Nygard.
 
 #import "CDClassFrameworkVisitor.h"
 
