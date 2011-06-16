22e91fc9cb4ebcc466ae46c9c2a836b0afce4c5e
Cédric Luthi <cedric.luthi@gmail.com>
2011-06-16 17:05:18 +0200

    Fix compilation with 10.6 SDK

--- dyld-info-compat.h
+++ dyld-info-compat.h
@@ -78,3 +78,11 @@ struct dyld_info_command {
 #define EXPORT_SYMBOL_FLAGS_HAS_SPECIALIZATIONS                 0x10
 
 #endif
+
+#if MAC_OS_X_VERSION_MAX_ALLOWED <= MAC_OS_X_VERSION_10_6
+
+// This is taken from loader.h on 10.7, so that I can still compile for 10.6.
+
+#define LC_DYLD_ENVIRONMENT 0x27 /* string for dyld to treat like environment variable */
+
+#endif
