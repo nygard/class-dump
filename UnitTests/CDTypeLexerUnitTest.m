a8f187eea2ddca1a6b133244d5f88cee56ed94a5
Steve Nygard <nygard@gmail.com>
2012-02-23 03:33:58 -0700

    Updated copyrights for 2012.

--- UnitTests/CDTypeLexerUnitTest.m
+++ UnitTests/CDTypeLexerUnitTest.m
@@ -1,5 +1,5 @@
 //  This file is part of class-dump, a utility for examining the Objective-C segment of Mach-O files.
-//  Copyright (C) 1997-1998, 2000-2001, 2004-2011 Steve Nygard.
+//  Copyright (C) 1997-1998, 2000-2001, 2004-2012 Steve Nygard.
 
 #import "CDTypeLexerUnitTest.h"
 
