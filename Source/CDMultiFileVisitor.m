nygard/class-dump

commit 2ccd217b07ba2667bbd523fa9ce793526f9bf897

Trying to understand and simplify this code.
Just generate forward references for protocols instead of imports.

Source/CDMultiFileVisitor.m
@@ -177,10 +177,10 @@
 - (void)buildClassFrameworks;
 {
     CDClassFrameworkVisitor *visitor = [[CDClassFrameworkVisitor alloc] init];
-    [visitor setClassDump:self.classDump];
+    visitor.classDump = self.classDump;
+    visitor.symbolReferences = self.symbolReferences;
+    
     [self.classDump recursivelyVisit:visitor];
-    self.symbolReferences.frameworkNamesByClassName = [visitor.frameworkNamesByClassName copy];
-    self.symbolReferences.frameworkNamesByProtocolName = [visitor.frameworkNamesByProtocolName copy];
 }
 
 - (void)generateStructureHeader;

Source/CDClassFrameworkVisitor.h
@@ -5,7 +5,7 @@
 
 #import "CDVisitor.h"
 
-// This builds up a dictionary mapping class names to a framework names, and protocol names to framework names.
+// This builds up a dictionary mapping class names to a framework names.
 // It is only used by CDMultiFileVisitor to generate individual imports when creating separate header files.
 
 // Some protocols appear in multiple frameworks.  This just records the last framework that contained a reference, which
@@ -15,9 +15,12 @@
 // If we follow framework dependancies, the earliest reference to NSCopying is CoreFoundation, but NSCopying is really
 // defined in Foundation.
 
+// But it turns out that we can just use forward references for protocols.
+
+@class CDSymbolReferences;
+
 @interface CDClassFrameworkVisitor : CDVisitor
 
-@property (readonly) NSMutableDictionary *frameworkNamesByClassName;    // NSString (class name)    -> NSString (framework name)
-@property (readonly) NSMutableDictionary *frameworkNamesByProtocolName; // NSString (protocol name) -> NSString (framework name)
+@property (strong) CDSymbolReferences *symbolReferences;
 
 @end

Source/CDClassFrameworkVisitor.m
@@ -8,6 +8,7 @@
 #import "CDMachOFile.h"
 #import "CDOCClass.h"
 #import "CDObjectiveCProcessor.h"
+#import "CDSymbolReferences.h"
 
 @interface CDClassFrameworkVisitor ()
 @property  NSString *frameworkName;
@@ -17,20 +18,8 @@
 
 @implementation CDClassFrameworkVisitor
 {
-    NSMutableDictionary *frameworkNamesByClassName;
-    NSMutableDictionary *frameworkNamesByProtocolName;
-    NSString *frameworkName;
-}
-
-- (id)init;
-{
-    if ((self = [super init])) {
-        frameworkNamesByClassName = [[NSMutableDictionary alloc] init];
-        frameworkNamesByProtocolName = [[NSMutableDictionary alloc] init];
-        frameworkName = nil;
-    }
-
-    return self;
+    CDSymbolReferences *_symbolReferences;
+    NSString *_frameworkName;
 }
 
 #pragma mark -
@@ -42,22 +31,12 @@
 
 - (void)willVisitClass:(CDOCClass *)aClass;
 {
-    if (self.frameworkName != nil) {
-        [frameworkNamesByClassName setObject:self.frameworkName forKey:aClass.name];
-    }
-}
-
-- (void)willVisitProtocol:(CDOCProtocol *)protocol;
-{
-    if (self.frameworkName != nil) {
-        [frameworkNamesByProtocolName setObject:self.frameworkName forKey:protocol.name];
-    }
+    [self.symbolReferences addClassName:aClass.name referencedInFramework:self.frameworkName];
 }
 
 #pragma mark -
 
-@synthesize frameworkNamesByClassName;
-@synthesize frameworkNamesByProtocolName;
-@synthesize frameworkName;
+@synthesize symbolReferences = _symbolReferences;
+@synthesize frameworkName = _frameworkName;
 
 @end

Source/CDSymbolReferences.h
@@ -5,24 +5,28 @@
 
 #import <Foundation/Foundation.h>
 
+// 1. Record which frameworks reference classes.
+// 2. Provide the appropriate framework name for a class, to be used to generate #imports -- i.e. #import <Foundation/NSArray.h>
+
 @interface CDSymbolReferences : NSObject
 
-@property (strong) NSDictionary *frameworkNamesByClassName;    // NSString (class name)    -> NSString (framework name)
-@property (strong) NSDictionary *frameworkNamesByProtocolName; // NSString (protocol name) -> NSString (framework name)
+// Build up the data.  Used by CDClassFrameworkVisitor.
+- (void)addClassName:(NSString *)name referencedInFramework:(NSString *)framework;
 
 - (NSString *)frameworkForClassName:(NSString *)className;
-- (NSString *)frameworkForProtocolName:(NSString *)protocolName;
+
+
+
+
 
 - (void)addClassName:(NSString *)className;
 - (void)removeClassName:(NSString *)className;
 
-- (void)addProtocolName:(NSString *)protocolName;
 - (void)addProtocolNamesFromArray:(NSArray *)protocolNames;
 
 @property (nonatomic, readonly) NSString *referenceString;
 
 - (void)removeAllReferences;
 - (NSString *)importStringForClassName:(NSString *)className;
-- (NSString *)importStringForProtocolName:(NSString *)protocolName;
 
 @end

Source/CDSymbolReferences.m
@@ -7,6 +7,8 @@
 
 @interface CDSymbolReferences ()
 
+@property (readonly) NSMutableDictionary *frameworkNamesByClassName;    // NSString (class name)    -> NSString (framework name)
+
 @property (readonly) NSMutableSet *classes;
 @property (readonly) NSMutableSet *protocols;
 
@@ -21,8 +23,7 @@
 
 @implementation CDSymbolReferences
 {
-    NSDictionary *frameworkNamesByClassName;
-    NSDictionary *frameworkNamesByProtocolName;
+    NSMutableDictionary *frameworkNamesByClassName;
     
     NSMutableSet *classes;
     NSMutableSet *protocols;
@@ -31,8 +32,7 @@
 - (id)init;
 {
     if ((self = [super init])) {
-        frameworkNamesByClassName = nil;
-        frameworkNamesByProtocolName = nil;
+        frameworkNamesByClassName = [[NSMutableDictionary alloc] init];
         
         classes = [[NSMutableSet alloc] init];
         protocols = [[NSMutableSet alloc] init];
@@ -45,25 +45,26 @@
 
 - (NSString *)description;
 {
-    return [NSString stringWithFormat:@"<%@:%p> frameworkNamesByClassName: %@, frameworkNamesByProtocolName: %@, classes: %@, protocols: %@",
+    return [NSString stringWithFormat:@"<%@:%p> frameworkNamesByClassName: %@, classes: %@, protocols: %@",
             NSStringFromClass([self class]), self,
-            self.frameworkNamesByClassName, self.frameworkNamesByProtocolName,
+            self.frameworkNamesByClassName,
             self.classesSortedByName, self.protocolsSortedByName];
 }
 
 #pragma mark -
 
 @synthesize frameworkNamesByClassName;
-@synthesize frameworkNamesByProtocolName;
+
+- (void)addClassName:(NSString *)name referencedInFramework:(NSString *)framework;
+{
+    if (name != nil && framework != nil) {
+        [self.frameworkNamesByClassName setObject:framework forKey:name];
+    }
+}
 
 - (NSString *)frameworkForClassName:(NSString *)className;
 {
     return [frameworkNamesByClassName objectForKey:className];
-}
-
-- (NSString *)frameworkForProtocolName:(NSString *)protocolName;
-{
-    return [frameworkNamesByProtocolName objectForKey:protocolName];
 }
 
 - (void)addClassName:(NSString *)className;
@@ -75,11 +76,6 @@
 {
     if (className != nil)
         [classes removeObject:className];
-}
-
-- (void)addProtocolName:(NSString *)protocolName;
-{
-    [protocols addObject:protocolName];
 }
 
 - (void)addProtocolNamesFromArray:(NSArray *)protocolNames;
@@ -117,19 +113,6 @@
     return nil;
 }
 
-- (NSString *)importStringForProtocolName:(NSString *)protocolName;
-{
-    if (protocolName != nil) {
-        NSString *framework = [self frameworkForProtocolName:protocolName];
-        if (framework == nil)
-            return [NSString stringWithFormat:@"#import \"%@-Protocol.h\"\n", protocolName];
-        else
-            return [NSString stringWithFormat:@"#import <%@/%@-Protocol.h>\n", framework, protocolName];
-    }
-
-    return nil;
-}
-
 #pragma mark -
 
 @synthesize classes;
@@ -147,18 +130,12 @@
 
 - (void)_appendToString:(NSMutableString *)resultString;
 {
-    NSArray *names = self.protocolsSortedByName;
-    for (NSString *name in names) {
-        NSString *str = [self importStringForProtocolName:name];
-        if (str != nil)
-            [resultString appendString:str];
+    if ([self.protocols count] > 0) {
+        [resultString appendFormat:@"@protocol %@;\n\n", [self.protocolsSortedByName componentsJoinedByString:@", "]];
     }
-    if ([names count] > 0)
-        [resultString appendString:@"\n"];
     
-    names = self.classesSortedByName;
-    if ([names count] > 0) {
-        [resultString appendFormat:@"@class %@;\n\n", [names componentsJoinedByString:@", "]];
+    if ([self.classes count] > 0) {
+        [resultString appendFormat:@"@class %@;\n\n", [self.classesSortedByName componentsJoinedByString:@", "]];
     }
 }
 

commit 1814af213c2852ef70ffa399f220915e70e8d6d8

Fix the #import names of protocols in -H mode.
Protocol declarations (@protocol ... @end) are generated in files
named according to the scheme "MyProtocolName-Protocol.h". But
sometime between September 2011 and May 2012, class-dump started
emitting the corresponding #import directives using the scheme
"MyProtocolName.h" --- missing the "-Protocol" part. With this
revision, I restore the old correct behavior.

I think this probably broke in revision 2ccd217b, judging by that
revision's commit message. :)

Source/CDMultiFileVisitor.m
@@ -235,9 +235,9 @@
     if (name != nil) {
         NSString *framework = [self frameworkForProtocolName:name];
         if (framework == nil)
-            return [NSString stringWithFormat:@"#import \"%@.h\"\n", name];
+            return [NSString stringWithFormat:@"#import \"%@-Protocol.h\"\n", name];
         else
-            return [NSString stringWithFormat:@"#import <%@/%@.h>\n", framework, name];
+            return [NSString stringWithFormat:@"#import <%@/%@-Protocol.h>\n", framework, name];
     }
     
     return nil;