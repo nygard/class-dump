8c05c6b563f3014cbe08b50b622128031b30d309
Steve Nygard <nygard@gmail.com>
2012-09-15 11:09:25 -0600

    Rename UnitTests to TestCDNameForCPUType.

---
+++ UnitTests/TestCDNameForCPUType.m
@@ -0,0 +1,29 @@
+// -*- mode: ObjC -*-
+
+//  This file is part of class-dump, a utility for examining the Objective-C segment of Mach-O files.
+//  Copyright (C) 1997-1998, 2000-2001, 2004-2012 Steve Nygard.
+
+#import "TestCDNameForCPUType.h"
+
+@implementation TestCDNameForCPUType
+
+- (void)setUp;
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown;
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample;
+{
+    STFail(@"Unit tests are not implemented yet in UnitTests", NULL);
+}
+
+@end
