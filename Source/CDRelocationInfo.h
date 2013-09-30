490b1f756765eb60b17305a7ca350d0ebaa5a6eb
Steve Nygard <nygard@gmail.com>
2013-09-29 23:42:53 -0600

    Update copyrights for 2013.

--- Source/CDRelocationInfo.h
+++ Source/CDRelocationInfo.h
@@ -1,7 +1,7 @@
 // -*- mode: ObjC -*-
 
 //  This file is part of class-dump, a utility for examining the Objective-C segment of Mach-O files.
-//  Copyright (C) 1997-1998, 2000-2001, 2004-2012 Steve Nygard.
+//  Copyright (C) 1997-1998, 2000-2001, 2004-2013 Steve Nygard.
 
 #include <mach-o/reloc.h>
 
--- Source/CDTopologicalSortProtocol.h
+++ Source/CDTopologicalSortProtocol.h
@@ -1,7 +1,7 @@
 // -*- mode: ObjC -*-
 
 //  This file is part of class-dump, a utility for examining the Objective-C segment of Mach-O files.
-//  Copyright (C) 1997-1998, 2000-2001, 2004-2012 Steve Nygard.
+//  Copyright (C) 1997-1998, 2000-2001, 2004-2013 Steve Nygard.
 
 // A rather clunky way to avoid warnings in CDTopoSortNode.m regarding -retain not implemented by protocols
 @protocol CDTopologicalSort <NSObject>
