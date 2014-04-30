fcdf27ddb199a8448082f6263fdd79bc6065059b
Kamil Trzciński <kamil.trzcinski@polidea.com>
2014-04-30 17:35:42 +0200

    Change the version to 0.5

--- Source/CDClassDump.h
+++ Source/CDClassDump.h
@@ -5,7 +5,7 @@
 
 #import "CDFile.h" // For CDArch
 
-#define CLASS_DUMP_BASE_VERSION "3.5 (64 bit)"
+#define CLASS_DUMP_BASE_VERSION "0.5 (64 bit)"
 
 #ifdef DEBUG
 #define CLASS_DUMP_VERSION CLASS_DUMP_BASE_VERSION " (Debug version compiled " __DATE__ " " __TIME__ ")"
