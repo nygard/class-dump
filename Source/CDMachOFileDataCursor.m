1161f74f97f86201804b71c036644f7210665e41
Mike Richter <mrichter@preemptive.com>
2020-10-06 11:49:32 -0400

    Support small methods and selector indirection

--- Source/CDMachOFileDataCursor.m
+++ Source/CDMachOFileDataCursor.m
@@ -110,4 +110,15 @@
     return 0;
 }
 
+- (uint64_t)readPtr:(bool)small;
+{
+    // "small" pointers are signed 32-bit values
+    if (small) {
+        // The pointers are relative to the location in the image, so get the offset before reading the offset:
+        return [self offset] + [self readInt32];
+    } else {
+        return [self readPtr];
+    }
+}
+
 @end
